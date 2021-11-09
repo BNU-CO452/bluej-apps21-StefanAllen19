69610ffd3b3bbf8f1fa4b944be775ae7f63229a0
StefanAllen19 <91545229+StefanAllen19@users.noreply.github.com>
2021-11-09 07:27:31 +0000

    app212

--- app213-products/StockDemo.java
+++ app213-products/StockDemo.java
@@ -23,16 +23,8 @@ public class StockDemo
         // Add at least 10 products, they must be unique to you
         // Make sure the ids are sequential numbers
         
-        stock.add(new Product(101, "BMW M8 Grand Coupe"));
-        stock.add(new Product(102, "Mercedes AMG GT Black Series"));
-        stock.add(new Product(103, "ABT Audi R8 V10 Performace Quattro"));
-        stock.add(new Product(104, "ABT RS3"));
-        stock.add(new Product(105, "Brabus E63S AMG"));
-        stock.add(new Product(106, "Lamborghini aventador SVJ"));
-        stock.add(new Product(107, "ABT RS6"));
-        stock.add(new Product(108, "Lamborghini hurrican performante"));
-        stock.add(new Product(109, "BMW M5 Competition"));
-        stock.add(new Product(110, "Brabus Rocket 900"));
+        stock.add(new Product(101, "RTX 3060"));
+        stock.add(new Product(102, "RTX 3060 TI"));
         
         runDemo();
     }
@@ -48,41 +40,19 @@ public class StockDemo
     {
         // Show details of all of the products before delivery.
         
-        stock.print();
-
-        buyProducts();
-        stock.print();        
-
-        sellProducts();
-        stock.print();        
+        stock.print();      
     }
     
     private void buyProducts()
     {
         stock.buyProduct(101, 9);
         stock.buyProduct(102, 4);
-        stock.buyProduct(103, 9);
-        stock.buyProduct(104, 3);
-        stock.buyProduct(105, 7);
-        stock.buyProduct(106, 5);
-        stock.buyProduct(107, 2);
-        stock.buyProduct(108, 9);
-        stock.buyProduct(109, 8);
-        stock.buyProduct(110, 1);
     }
 
     private void sellProducts()
     {
         stock.sellProduct(101, 5);
         stock.sellProduct(102, 2);
-        stock.sellProduct(103, 4);
-        stock.sellProduct(104, 1);
-        stock.sellProduct(105, 5);
-        stock.sellProduct(106, 4);
-        stock.sellProduct(107, 1);
-        stock.sellProduct(108, 6);
-        stock.sellProduct(109, 4);
-        stock.sellProduct(110, 2);
     }    
     
 }
