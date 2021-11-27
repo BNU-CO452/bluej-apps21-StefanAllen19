a5c05e78aed3e6700499cdadae7b759cb55c477e
stefab <22135474@bucks.ac.uk>
2021-11-27 19:00:53 +0000

    app214

--- app214-stock/StockApp.java
+++ app214-stock/StockApp.java
@@ -12,7 +12,7 @@ public class StockApp
     private InputReader reader;
     
     private StockList stock;
-    
+
     /**
      * Constructor for objects of class StockApp
      */
@@ -21,7 +21,7 @@ public class StockApp
         reader = new InputReader();
         
         stock = new StockList();
-        //StockDemo demo = new StockDemo(stock);
+        StockDemo demo = new StockDemo(stock);
     }
 
     /**
